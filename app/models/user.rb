7d9dd810318478a98b3911b60e8b12832b890643
Daria Korytova <dk17march@gmail.com>
2020-07-08 23:42:41 +0300

    activerecord corrections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  def show_tests_by_level(level)
-    UserTest.joins("JOIN tests ON test.id = useer.test_id")
-    .where("user_id = ? AND tets.level = ?", user_id, level)
+  def user_tests_by_level(level)
+    Test.joins('LEFT JOIN usertests ON tests.id = usertests.test_id')
+        .where('tests.level = :level AND usertests.user_id = :user_id', level: level, user_id: id)
+        .joins('LEFT JOIN statuses ON usertests.status_id = statuses.id')
+        .where('usertests.status_id >= :status', status: 2)
   end
 end
