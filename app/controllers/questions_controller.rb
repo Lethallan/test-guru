c62f09a2ecd612700831cf7d1ab3fa3897532bac
Daria Korytova <dk17march@gmail.com>
2020-07-20 02:58:01 +0300

    rest corrections

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -36,14 +36,14 @@ class QuestionsController < ApplicationController
   end
 
   def find_test
-    @test = Test.find(params[:id])
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
     @question = Question.find(params[:id])
   end
 
-  def rescue_with_question_not_find
+  def rescue_with_question_not_found
     render plain: 'No such question'
   end
 end
