707bce618af31a62f5c56ad405f373d9517dcbcd
Daria Korytova <dk17march@gmail.com>
2020-07-31 18:18:56 +0300

    app logic task

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,10 +1,14 @@
 class TestPassagesController < ApplicationController
   before_action :set_test_passage, only: %i[show update result]
+  before_action :set_current_question_number, only: :show
+  before_action :count_questions, only: %i[show result]
+
 
   def show
   end
 
   def result
+    @percentage = @test_passage.correct_questions / @count_questions.to_f * 100
   end
 
   def update
@@ -21,5 +25,16 @@ class TestPassagesController < ApplicationController
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def set_current_question_number
+    @current_question_number = @test_passage.test.questions.index(@test_passage.current_question) + 1
+  end
+
+  def count_questions
+    @count_questions = @test_passage.test.questions.count
+  end
+
+  def percentage
   end
 end
