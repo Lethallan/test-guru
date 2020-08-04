6508808e6454d6572dae18c68620a73490bcad21
Daria Korytova <dk17march@gmail.com>
2020-08-04 20:08:37 +0300

    auth lesson 1/2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
+require 'digest/sha1'
+require 'bcrypt'
+
 class User < ApplicationRecord
-    # def user_tests_by_level(level)
-  #   Test.joins('LEFT JOIN usertests ON tests.id = usertests.test_id')
-  #       .where('tests.level = :level AND usertests.user_id = :user_id', level: level, user_id: id)
-  #       .joins('LEFT JOIN statuses ON usertests.status_id = statuses.id')
-  #       .where('usertests.status_id >= :status', status: 2)
-  # end
+
+  include BCrypt
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :user_id
 
-  validates :name, :email, presence: true, uniqueness: true
+  has_secure_password
+
+  # validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  # validates :password, confirmation: true
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
