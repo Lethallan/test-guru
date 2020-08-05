82300219b302aaa27ee2bb4d743ef156d1ee6981
Daria Korytova <dk17march@gmail.com>
2020-08-05 23:05:45 +0300

    auth lesson task init

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,7 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_test, only: %i[show destroy edit update start]
   before_action :set_user, only: :start
+  before_action :find_test, only: %i[show destroy edit update start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -64,7 +64,6 @@ class TestsController < ApplicationController
   end
 
   def set_user
-    @user = User.first
-    #Перестало работать прохождение теста после добавления аутентификации... похоже, тут конфликт.
+    @user = @current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # validates :name, presence: true
-  # validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   # validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
   # validates :password, confirmation: true
 
