fc533f5f896ca26c010e6e8ea94c463a8a4f1588
Daria Korytova <dk17march@gmail.com>
2020-08-19 20:55:09 +0300

    corrections in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,6 @@ Rails.application.routes.draw do
   # resources :sessions, only: :create
 
   resources :tests, only: :index do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
-
     member do
       post :start
     end
@@ -35,7 +31,7 @@ Rails.application.routes.draw do
     resources :tests do
       resources :questions, shallow: true, except: :index do
         resources :answers, shallow: true, except: :index
-        end
       end
+    end
   end
 end
