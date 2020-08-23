b219a88c79011c610fc53d2fce667c244943babe
Daria Korytova <dk17march@gmail.com>
2020-08-24 01:08:20 +0300

    roots corrections

--- config/routes.rb
+++ config/routes.rb
@@ -15,16 +15,12 @@ Rails.application.routes.draw do
   # resources :sessions, only: :create
 
   resources :tests, only: :index do
-    member do
-      post :start
-    end
+    post :start, on: :member
   end
 
   #GET /test_passages/101/result
   resources :test_passages, only: %i[show update] do
-    member do
-      get :result
-    end
+    get :result, on: :member
   end
 
   namespace :admin do
