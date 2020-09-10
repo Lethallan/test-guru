a2241b4e082130041f2e47d3a56f41f1ecf1f5bb
Daria Korytova <dk17march@gmail.com>
2020-09-10 12:59:23 +0300

    try to fix deploy errors again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'lit-ocean-70354.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'lit-ocean-70354.herokuapp.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
